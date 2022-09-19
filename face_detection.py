bef182cbbb599431d75dc7acf4eeb15c4f3a2936
Dheeraj Kashyap <dheerajkashyap@wireless-10-146-62-7.public.utexas.edu>
2022-09-19 16:37:35 -0500

    added comments

--- face_detection.py
+++ face_detection.py
@@ -1,19 +1,22 @@
 import cv2
 
+# obtain trained facial data from OpenCV dataset
 open_cv_trained_model = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
+# use webcam as input
 webcam_input = cv2.VideoCapture(0)
 
+#infinite loop
 while True:
     read_frame_success, frame = webcam_input.read()
-    greyscale_conversion = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    greyscale_conversion = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #convert to greyscaled
     data_points_face = open_cv_trained_model.detectMultiScale(greyscale_conversion)
-    for (x, y, w, h) in data_points_face:
+    for (x, y, w, h) in data_points_face: #loop through data points to create rectangle for facial recognition
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0,85,204), 10)
-    cv2.imshow('Face Detection Using OpenCV', frame)
-    key_input = cv2.waitKey(1)
+    cv2.imshow('Face Detection Using OpenCV', frame) #displays face detection
+    key_input = cv2.waitKey(1) #store key input
 
-    if key_input == 81 or key_input == 113: 
+    if key_input == 81 or key_input == 113: #if q is pressed, exit loop
         break
 
-webcam_input.release()
+webcam_input.release() #terminate program
